6a545fad17fa236c2677dc6c98cf3ef9c37ace90
Joshua Petitt <joshpetitt@jpmec.com>
2013-05-20 19:33:25 -0500

    Misc tweaks

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,14 +1,10 @@
 class PicsController < ApplicationController
   #include RestfulJson::DefaultController
 
-  # GET /screencasts
-  # GET /screencasts.json
   def index
     render json: Pic.all
   end
 
-  # GET /screencasts/:id
-  # GET /screencasts/:id.json
   def show
     render json: Pic.find(params[:id])
   end
